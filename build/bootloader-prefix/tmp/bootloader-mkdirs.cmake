dff46a281d6d51b797a5a1b34073fdceb25910b1
nightcatccc <2647687276@qq.com>
2025-03-09 18:17:44 +0800

    屏幕显示、触摸正常

---
+++ build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "D:/esp32-idf/v5.2.3/esp-idf/components/bootloader/subproject"
+  "D:/esp32-idf/esp-pro/MY_PRO/LCD/spi_lcd_touch/build/bootloader"
+  "D:/esp32-idf/esp-pro/MY_PRO/LCD/spi_lcd_touch/build/bootloader-prefix"
+  "D:/esp32-idf/esp-pro/MY_PRO/LCD/spi_lcd_touch/build/bootloader-prefix/tmp"
+  "D:/esp32-idf/esp-pro/MY_PRO/LCD/spi_lcd_touch/build/bootloader-prefix/src/bootloader-stamp"
+  "D:/esp32-idf/esp-pro/MY_PRO/LCD/spi_lcd_touch/build/bootloader-prefix/src"
+  "D:/esp32-idf/esp-pro/MY_PRO/LCD/spi_lcd_touch/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "D:/esp32-idf/esp-pro/MY_PRO/LCD/spi_lcd_touch/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "D:/esp32-idf/esp-pro/MY_PRO/LCD/spi_lcd_touch/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
